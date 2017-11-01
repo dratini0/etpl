4ebebce3b1ed2bd2f42c9ae8b1e680b823a84638
Balint Kovacs <dratini0@gmail.com>
2017-11-01 13:43:17 +0000

    Rationalize build process by only outputting AMD

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: __dirname + "/lib/js/src/frontend/frontend.js",
+    entry: __dirname + "/lib/amdjs/src/frontend/frontend.js",
     output: {
         path: __dirname + '/build/bundle/js',
         filename: 'bundle.js'
