75b6dfa1f04e67c0fa063e2e768247c579cf8808
Balint Kovacs <dratini0@gmail.com>
2017-11-20 18:17:39 +0000

    Implement CORS for the loggin API

--- logServer/logServer.py
+++ logServer/logServer.py
@@ -10,6 +10,7 @@ from flask import request, g
 from flask_api import FlaskAPI, status, exceptions
 import jsonschema
 import jwt
+from flask_cors import CORS
 
 JWT_SECRET = os.urandom(16) # offload to config file
 JWT_VALIDITY = datetime.timedelta(days=1)
@@ -30,6 +31,7 @@ VALUES (?, ?, ?, ?)
 '''
 
 app = FlaskAPI(__name__)
+CORS(app)
 
 def get_db():
     db = getattr(g, '_database', None)
