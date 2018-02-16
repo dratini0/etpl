adfbc32c1befcfc64e7a2204d6af7e9c2372b15d
Balint Kovacs <dratini0@gmail.com>
2018-02-16 00:30:06 +0000

    Add rendering of pairs

--- src/frontend/codeRender.ml
+++ src/frontend/codeRender.ml
@@ -20,16 +20,23 @@ let posToId position = "node" ^ (position
                                   |> List.map string_of_int
                                   |> String.concat "_")
 
-let renderValue = function
-  | Number(n) -> (Jquery.jquery'' (createTextNode (string_of_float n)))
-  | String(s) -> (Jquery.jquery'' (createTextNode s))
+let rec renderValue = function
+  | Number(n) ->
+      cloneElementFromTemplate "literal_Number"
+        |> setChild 0 (Jquery.jquery'' (createTextNode (string_of_float n)))
+  | String(s) ->
+      cloneElementFromTemplate "literal_String"
+        |> setChild 0 (Jquery.jquery'' (createTextNode s))
+  | Pair(v1, v2) ->
+      cloneElementFromTemplate "literal_Pair"
+        |> setChild 0 (renderValue v1)
+        |> setChild 1 (renderValue v2)
+  | Array _ ->
+      raise Exit
 
 let rec renderExpression expression position specialCasingFunction = begin
   let element = (match expression with
-    | Literal value ->
-      let typeName_ = typeName (inferTypeValue value) in
-      cloneElementFromTemplate ("literal_" ^ typeName_)
-        |> setChild 0 (renderValue value)
+    | Literal value -> renderValue value
     | Constant(c) ->
       cloneElementFromTemplate ("constant_" ^ (constantName c))
     | UnaryOp(o, e0) ->
